a51be9db03c0970a5f6ed56456bb77ec50bd7260
Matthieu Totet <matthieu.totet@gmail.com>
2014-11-09 14:13:26 +0100

    03 Success (with luck)

---
+++ 03 - Largest prime factor/main.py
@@ -0,0 +1,14 @@
+number = 600851475143 
+import math
+def isPrime(n):
+    for j in range(int(n/2)+1,2,-1):
+        if n % j == 0:
+            return False   
+    return True  
+i = int(math.sqrt(number))
+while i:
+    if number % i == 0:
+        if isPrime(i):
+            print i
+            exit()
+    i -=1
