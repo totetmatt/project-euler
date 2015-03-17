26d15a4e751bdc69959a248734b2e7a357f96b49
Matthieu Totet <matthieu.totet@gmail.com>
2015-03-17 22:11:31 +0100

    Solved

---
+++ 09 - Special Pythagorean triplet/main.py
@@ -0,0 +1,9 @@
+import itertools
+import math
+
+p = [r*r for r in range(1,1000)]
+for a in itertools.permutations(p, 2):
+    if a[0] + a[1] in p:
+        if  math.sqrt(a[0]) + math.sqrt(a[1]) + math.sqrt(a[0] + a[1]) == 1000:
+            print int(math.sqrt(a[0]) * math.sqrt(a[1]) * math.sqrt(a[0] + a[1]))
+            exit()
